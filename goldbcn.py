baf655ea94852540d15052ba8d7f973ea763ed90
Ying YinPo <inpool@126.com>
2016-10-22 14:54:48 +0800

    将数据来源从有道词典网页改为有道词典api(XML)，移除第三方依赖

--- goldbcn.py
+++ goldbcn.py
@@ -1,6 +1,8 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 import os
+import urllib2
+import sqlite3
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 DB_PATH = os.path.expanduser('~/.goldendict/dbcn/dict.db')
@@ -10,16 +12,18 @@ TEMPLATE_TXT = os.path.join(DIR_PATH, 'res/template.txt')
 def get_db():
     if hasattr(get_db, 'db'):
         return get_db.db
-    import sqlite3
-    db = sqlite3.connect(DB_PATH)
+
+    db = sqlite3.connect(DB_PATH)  # @UndefinedVariable
+
     get_db.db = db
     return db
+
 
 def lookup(word):
     word = word.strip()
     if not word.isalpha():
         try:
-            result = lookup_sentence(word)
+            result = lookup_from_net(word)
         except LookupNetException:
             return None
         return result
@@ -27,21 +31,18 @@ def lookup(word):
     result = lookup_from_db(word)
     if result:
         return result
-    try:
-        html = response_from_net(word)
-    except LookupNetException:
-        db.close()
-        return None
-    result = result_from_html(html)
+    result = lookup_from_net(word)
     save_into_db(result)
     return result
 
-def lookup_sentence(word):
+
+def lookup_from_net(word):
     try:
-        html = response_from_net(word)
+        html = xml_from_net(word)
     except LookupNetException:
         return None
-    return result_from_html(html)
+    return result_from_xml(html)
+
 
 def lookup_from_db(word):
     db = get_db()
@@ -53,50 +54,53 @@ def lookup_from_db(word):
     if result:
         return tuple(i.encode('utf-8') for i in result)
 
-def response_from_net(word):
+
+def xml_from_net(word, lookup_url=None):
     word = quote_item(word)
-    lookup_url = 'http://dict.youdao.com/search?q=' + word
-    import urllib2
+    if lookup_url is None:
+        lookup_url = ('http://dict.youdao.com/fsearch?q=%s&doctype=xml'
+                      '&xmlVersion=3.2&le=eng&pos=-1')
+    lookup_url = lookup_url % word
     try:
-        html = urllib2.urlopen(lookup_url).read()
+        xml = urllib2.urlopen(lookup_url).read()
     except urllib2.HTTPError as e:
-        raise LookupNetException(word, e,geturl())
-    return html
+        raise LookupNetException(word, e, lookup_url)
+    return xml
+
 
 def quote_item(word):
     safe_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRST0123456789-_.~"
     if isinstance(word, unicode):
         word = word.encode('utf8')
+
     def quote_char(char):
         if char not in safe_chars:
             char = '%%%X' % ord(char)
         return char
     return ''.join(map(quote_char, word))
 
-def result_from_html(s):
-    from lxml.html import document_fromstring
-    document = document_fromstring(s)
-    phrs = document.cssselect('#phrsListTab')
-    if not phrs:
-        return None
-    phrs = phrs[0]
 
-    keyword = phrs.cssselect('span.keyword')[0].text.decode('utf8')
-    sound = phrs.cssselect('div.baav')
-    means = phrs.cssselect('div.trans-container li')
+def result_from_xml(s):
+    from xml.etree import ElementTree
+    et = ElementTree.fromstring(s)
+    means_els = et.findall('./custom-translation/translation/content')
+    if not means_els:
+        web_path = "./yodao-web-dict/web-translation[@same='true']/trans/value"
+        means_els = et.findall(web_path)
+        if not means_els:
+            return
+    means = '\n'.join(e.text.strip().encode('utf-8') for e in means_els)
+    keyword = et.find('./return-phrase').text.strip().encode('utf-8')
+    uk_sound = et.find('./uk-phonetic-symbol')
+    us_sound = et.find('./us-phonetic-symbol')
+    sounds = []
+    if uk_sound is not None:
+        sounds.append('英 [%s] ' % uk_sound.text.strip().encode('utf-8'))
+    if us_sound is not None:
+        sounds.append('美 [%s] ' % us_sound.text.strip().encode('utf-8'))
+    sound = ''.join(sounds)
+    return keyword, sound, means
 
-    if sound:
-        import re
-        sound = sound[0].text_content().strip()
-        sound = ' '.join(re.split(r'\s+', sound))
-    else:
-        sound = ''
-    means = [i.text for i in means]
-    while None in means:
-        means.remove(None)
-    means = '\n'.join(means).strip()
-
-    return keyword.encode('utf8'), sound.encode('utf8'), means.encode('utf8')
 
 def save_into_db(result):
     word, sound, mean = [i.decode('utf-8') for i in result]
@@ -113,9 +117,11 @@ def save_into_db(result):
     cursor.execute(sql % table_name, (word.lower(), word, sound, mean))
     db.commit()
 
+
 def get_table_name(word):
     word = word.lower()
     return 'word_%s' % (word[0] + word[-1])
+
 
 def to_txt(result):
     ERROR_MSG = '单词未找到！'
@@ -126,7 +132,8 @@ def to_txt(result):
 
 
 class LookupNetException(Exception):
+
     def __init__(self, word, url):
-        self.word = wor
+        self.word = word
         self.message = 'Lookup word "%s: faild!' % word
         self.url = url
